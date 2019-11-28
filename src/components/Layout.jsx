526947ac8e68fa81c79c1bc1708377fa0038b97f
vicc91 <vlroa30@gmail.com>
2019-11-28 18:07:00 -0500

    Add responsive desing breakpoint 768px, and animation main menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import MainMenu from './MainMenu';
 
@@ -8,11 +8,21 @@ const Layout = ({
     addCategory,
     selectCategory,
 }) => {
-    const [activeMenu, setActiveMenu] = useState(true);
+    const [activeMenu, setActiveMenu] = useState(window.innerWidth <= 768 ? false : true);
 
     const toggleMenu = () => {
         setActiveMenu(!activeMenu);
     }
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            if (window.innerWidth <= 768) {
+                setActiveMenu(false);
+            } else {
+                setActiveMenu(true);
+            }
+        });
+    }, []);
 
     return (
         <div className="Layout">
